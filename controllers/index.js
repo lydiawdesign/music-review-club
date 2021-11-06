5ecd8d6cbb1a81638520e43133354d6d53ed050d
Shawn Miller <shawnsmm@gmail.com>
2021-11-05 22:49:17 -0400

    updated controller index with correct routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,10 @@
 const router = require('express').Router();
 
-// const apiRoutes = require('./api');
-const homeRoutes = require('./homeroute');
-// const dashboardRoutes = require('./dashboard');
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeroutes');
 
-// router.use('/api', apiRoutes);
-// router.use('/dashboard', dashboardRoutes);
+
+router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
 module.exports = router;
